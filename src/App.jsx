d31bf9a45d8087ca8c7ff64f4db80afaf32bbd91
Yashika Khandelwal <kyashika.308@gmail.com>
2025-11-21 10:06:05 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,101 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
+import {
+  FormControl, InputGroup, 
+  Container, Button
+} from "react-bootstrap";
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET
 
 function App() {
-  
+
+  const [searchInput, setSeachInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState("");
+
+  useEffect(() => {
+  let authParams = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body:
+      "grant_type=client_credentials&client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret,
+  };
+
+  fetch("https://accounts.spotify.com/api/token", authParams)
+    .then((result) => result.json())
+    .then((data) => {
+      setAccessToken(data.access_token);
+    });
+  }, []);
+
+  async function search() {
+  let artistParams = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessToken,
+    },
+  };
+
+  // Get Artist
+  const artistID = await fetch(
+    "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+    artistParams
+  )
+    .then((result) => result.json())
+    .then((data) => {
+      return data.artists.items[0].id;
+    });
+
+     // Get Artist Albums
+    await fetch(
+    "https://api.spotify.com/v1/artists/" +
+      artistID +
+      "/albums?include_groups=album&market=US&limit=50",
+    artistParams
+  )
+    .then((result) => result.json())
+    .then((data) => {
+      setAlbums(data.items);
+    });
+
+    console.log("Search Input: " + searchInput);
+    console.log("Artist ID: " + artistID);
+  }
 
   return (
    <>
-      <h1>Codédex Project Template</h1>
+      <Container>
+        <InputGroup>
+          <FormControl 
+            placeholder='Search for Artist'
+            type='input'
+            aria-label='Search for Artist'
+            onKeyDown={(event) => {
+              if (event.key === "Enter"){
+                search();
+              }
+            }}
+            onChange={(event) => setSeachInput(event.target.value)}
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px"
+            }}/>
+            <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
     </>
   )
 }
